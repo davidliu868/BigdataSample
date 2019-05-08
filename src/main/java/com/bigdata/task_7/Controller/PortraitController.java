d1f042f7ca43540818b8be9885fd681e573ff289
liuxibiao <41683283@qq.com>
2019-05-08 20:38:36 +0800

    去掉测试目录

---
+++ src/main/java/com/bigdata/task_7/Controller/PortraitController.java
@@ -0,0 +1,14 @@
+package com.bigdata.task_7.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PortraitController {
+
+    @RequestMapping(method = RequestMethod.GET,produces = "application/json;charset=UTF-8",value="/hello")
+    public String say(){
+        return "hello胜多负少的";
+    }
+}
---
+++ src/main/java/com/bigdata/task_7/a.java
@@ -0,0 +1,4 @@
+package com.bigdata.task_7;
+
+public class a {
+}
